56907908a1a0d0f74cafc11fd9bc76fb40b0eed5
r3d5un <r3d5un@pm.me>
2023-07-16 01:56:49 +0200

    Fix typo

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -15,7 +15,7 @@ func secureHeaders(next http.Handler) http.Handler {
 			"default-src 'self'; style-src 'self' fonts.googleapis.com; font-src fonts.gstatic.com",
 		)
 
-		w.Header().Set("Referer-Policy", "origin-when-cross-origin")
+		w.Header().Set("Referrer-Policy", "origin-when-cross-origin")
 		w.Header().Set("X-Content-Type-Options", "nosniff")
 		w.Header().Set("X-Frame-Options", "deny")
 		w.Header().Set("X-XSS-Protection", "0")
